4438a1745574f0adaa294b257045e58897ad3866
Yasmine Elnadi <yasmineelnadi@Mohameds-MacBook-Pro.local>
2022-10-19 11:36:02 -0700

    Phase-0 complete

---
+++ backend/routes/index.js
@@ -0,0 +1,12 @@
+//create an Express router, create a test route, and export the router at the bottom of the file.
+
+// backend/routes/index.js
+const express = require('express');
+const router = express.Router();
+
+router.get('/hello/world', function(req, res) {
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  res.send('Hello World!');
+});
+
+module.exports = router;
