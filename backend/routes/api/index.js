47cdd628d893379becab8d4dbae573ea5ce612b3
Yasmine Elnadi <yasmineelnadi@Mohameds-MacBook-Pro.local>
2022-10-20 14:00:07 -0700

    Add User authentication Express middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,57 @@
 const router = require('express').Router();
 module.exports = router;
+const { restoreUser } = require("../../utils/auth.js");
 
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
+
+
+
+//test user auth middlewares
+// GET /api/set-token-cookie
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+// router.get('/set-token-cookie', async (_req, res) => {
+//   const user = await User.findOne({
+//       where: {
+//         username: 'Demo-lition'
+//       }
+//     });
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// });
+
+
+// GET /api/restore-user
+router.use(restoreUser);
+router.get(
+  '/restore-user',
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+//test requireAuth
+// router.use(restoreUser);
+
+// // ...
+
+// // GET /api/require-auth
+// const { requireAuth } = require('../../utils/auth.js');
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+
+
+
+
+router.use(restoreUser);
+
+module.exports = router;
---
+++ backend/utils/auth.js
@@ -0,0 +1,75 @@
+// backend/utils/auth.js
+const jwt = require('jsonwebtoken');
+const { jwtConfig } = require('../config');
+const { User } = require('../db/models');
+
+const { secret, expiresIn } = jwtConfig;
+
+//sends a JWT Cookie
+const setTokenCookie = (res, user) =>{
+    //Create th Token
+    const token = jwt.sign(
+        {
+            data:user.toSafeObject()
+        },
+        secret,
+        {
+            expiresIn:parseInt(expiresIn)
+        }
+    );
+    const isProduction = process.env.NODE_ENV === "production";
+
+    //set the token cookie
+    res.cookie('token', token, {
+        maxAge: expiresIn * 1000,
+        httpOnly:true,
+        secure: isProduction,
+        sameSite: isProduction && "Lax"
+    });
+    return token;
+}
+
+//restoreUser
+
+const restoreUser = (req, res, next)=>{
+    //token parsed from cookies
+    const{token} = req.cookies;
+    req.user = null;
+
+    return jwt.verify(token, secret, null, async(err, jwtPayload)=>{
+        if(err){
+            return next();
+        }
+        try{
+            const{id} = jwtPayload.data;
+            req.user = await User.scope('currentUser').findByPk(id);
+
+        }catch(e){
+            res.clearCookie('token');
+            return next();
+        }
+        if(!req.user) res.clearCookie('token');
+        return next();
+    })
+}
+
+
+//requireAuth
+//if there is no current user return an error
+const requireAuth = function(req, _res,next) {
+    if(req.user) return next();
+
+    const err = new Error ('Unauthorized');
+    err.title = 'Unauthorized';
+    err.errors = ['Unauthorized'];
+    err.status = 401;
+    return next(err);
+}
+
+
+
+
+
+
+
+module.exports = { setTokenCookie, restoreUser, requireAuth };
