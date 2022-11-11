5de85abfe68b2a45181ce83dbea1cd6a192e9ce4
Yasmine Elnadi <yasmineelnadi@Mohameds-MacBook-Pro.local>
2022-11-11 15:13:35 -0800

    Authme phase 1

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,10 +1,15 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import sessionReducer from './session';
+import {login} from './session'
+
 
 
 
 
 const rootReducer = combineReducers({
+    session: sessionReducer,
+
 });
 
 
