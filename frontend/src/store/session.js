ecce2b7cacc88ac4272259709d7ceab7878634f5
Yasmine Elnadi <yasmineelnadi@Mohameds-MacBook-Pro.local>
2022-11-13 14:01:30 -0800

    logout action

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -64,6 +64,14 @@ export const login = (user) => async (dispatch) => {
     return response;
   };
 
+  export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+      method: 'DELETE',
+    });
+    dispatch(removeUser());
+    return response;
+  };
+
   
   
   // window.store.dispatch(window.sessionActions.signup({
