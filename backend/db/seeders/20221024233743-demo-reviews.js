588f0ce003c87dab2ae66b260f48fd36065c9db2
Yasmine Elnadi <yasmineelnadi@Mohameds-MacBook-Pro.local>
2022-11-21 06:54:53 -0800

    update seeders different reviewer than owner

--- backend/db/seeders/20221024233743-demo-reviews.js
+++ backend/db/seeders/20221024233743-demo-reviews.js
@@ -15,31 +15,31 @@ module.exports = {
    return queryInterface.bulkInsert("Reviews",[
      {
       spotId:1,
-      userId:1,
-      review: "This was an awesome spot!",
-      stars: 5
-     },
-     {
-      spotId:2,
       userId:2,
       review: "This was an awesome spot!",
       stars: 5
      },
      {
-      spotId:3,
+      spotId:2,
       userId:3,
       review: "This was an awesome spot!",
       stars: 5
      },
      {
-      spotId:4,
+      spotId:3,
       userId:4,
+      review: "This was an awesome spot!",
+      stars: 5
+     },
+     {
+      spotId:4,
+      userId:5,
       review: "This was an awesome spot!",
       stars: 5
      },
     {
       spotId:5,
-      userId:5,
+      userId:6,
       review: "This was an awesome spot!",
       stars: 5
     }
