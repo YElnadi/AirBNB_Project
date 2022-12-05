09801015a1f09738dddaa7d53d2754f32220e768
Yasmine Elnadi <yasmineelnadi@Mohameds-MacBook-Pro.local>
2022-12-04 23:50:25 -0800

    create spot form validation

--- frontend/src/components/CreateSpotModel/index.js
+++ frontend/src/components/CreateSpotModel/index.js
@@ -16,10 +16,38 @@ const CreateSpotModel = () => {
     const [description, setDescription] = useState('')
     const [price, setPrice] = useState('')
     const [imageUrl, setImageUrl] = useState('')
+    const [validationErrors, setValidationErrors] = useState([])
+    const [hasSubmitted, setHasSubmited] = useState(false)
     const { closeModal } = useModal();
 
 
+
     const sessionUser = useSelector(state => state.session.user);
+
+    useEffect(()=>{
+      const errors = []
+      if(!address) errors.push('Spot must have an address')
+      if(address.length<10) errors.push('please enter full address')
+      if(!state) errors.push('Spot must have a state')
+      if(state.length<2) errors.push('Please enter state name not less than 1 characters')
+      if(!city) errors.push('Spot must have a city')
+      if(city.length<2) errors.push('Please enter the city name not less than 1 character')
+      if(!country) errors.push('Spot must have a country')
+      if(country.length<2) errors.push('Please enter a country name not less than 1 character')
+      if(!name) errors.push('Spot must have a name')
+      if(name.length>50) errors.push('Please enter a name that is less than 50 characters')
+      if(!description) errors.push('Spot must have a description')
+      if(description.length<10) errors.push('Please descripe your spot not less than 10 characters')
+      if(description.length>5000) errors.push('Please descripe your spot not greather than 5000 characters')
+      if(!price) errors.push('Spot must have a price')
+      if(price<0) errors.push('Please enter a price that is greater than 0')
+      if(imageUrl.length === 0) errors.push('Please provide images for yout sopt so that your renters know how your spot looks like')
+      
+      setValidationErrors(errors)
+
+    },[name,city,state,country,address,description,price,imageUrl])
+
+   
     
     const cancel = async (e) =>{
         closeModal() 
@@ -29,13 +57,14 @@ const CreateSpotModel = () => {
       }
 
     if(!sessionUser){
-      history.push('/')
       window.alert('must be logged in')
+      history.push('/')
       closeModal()
     }
 
     const handleSubmit = async (e) =>{
         e.preventDefault();
+        setHasSubmited(true);
         const spotDetails={
             address,
             state,
@@ -66,6 +95,8 @@ const CreateSpotModel = () => {
         setName('');
         setPrice('');
         setImageUrl('');
+        setValidationErrors([]);
+        setHasSubmited(false);
 
     }
 
@@ -73,6 +104,16 @@ const CreateSpotModel = () => {
   return (
     <div>
         <h2>Create your spot</h2>
+        {hasSubmitted && validationErrors.length>0 && (
+          <div>
+            the following errors were found:
+            <ul>
+              {validationErrors.map((error)=>(
+                <li key={error}>{error}</li>
+              ))}
+            </ul>
+          </div>
+        )}
         <form onSubmit={handleSubmit}>
             <label>
               Address
@@ -127,7 +168,7 @@ const CreateSpotModel = () => {
 
             <label >
               Description
-            <input
+            <textarea
             type='text'
             value={description}
             onChange={(e)=>setDescription(e.target.value)}
@@ -156,7 +197,9 @@ const CreateSpotModel = () => {
             </label>
 
             
-            <button type='submit'> Save</button>
+            <button type='submit' 
+            disabled= {validationErrors.length>0}
+            > Save</button>
             <button onClick={cancel}> Cancel</button>
             
 
