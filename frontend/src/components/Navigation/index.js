897bb4f79413532e3ba3f7bfcd649b904f2aa550
Yasmine Elnadi <yasmine_adel@hotmail.com>
2022-12-09 11:34:39 -0800

    fix nav styling

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -37,30 +37,56 @@ function Navigation({ isLoaded }){
 
 
   return (
-    <div className='nav'>
-    <div style={{display:'flex',justifyContent:'space-between'}}>
-      <div style={{listStyle:'none'}}>
-        <NavLink exact to="/" style={{textDecoration:'none'}} id='logo' >
-          <img src ={image} style={{width:120, marginTop:10, cursor:'pointer',marginLeft:30}} alt='logo'/>
-        </NavLink>
-      </div>
+    // <div className='nav'>
+    // <div style={{display:'flex',justifyContent:'space-between'}}>
+    //   <div style={{listStyle:'none'}}>
+    //     <NavLink exact to="/" style={{textDecoration:'none'}} id='logo' >
+    //       <img src ={image} style={{width:120, marginTop:10, cursor:'pointer',marginLeft:30}} alt='logo'/>
+    //     </NavLink>
+    //   </div>
       
-      <div style={{marginTop:10, marginLeft:900, listStyle:'none',fontWeight:'bold'}} className='yasbnbBut' postion='absolute' id='btn'>
-        {/* <NavLink to ='/spots' style={{textDecoration:'none', color:'black', fontWeight:'bold'}}> */}
-        <OpenModalMenuItem
-                    itemText="Yasbnb your home"
-                    onItemClick={closeMenu}
-                    modalComponent={<CreateSpotModal />}
-                  /> 
-       {/* </NavLink> */}
-        </div>
+    //   <div style={{marginTop:10, marginLeft:900, listStyle:'none',fontWeight:'bold'}} className='yasbnbBut' postion='absolute' id='btn'>
+    //     {/* <NavLink to ='/spots' style={{textDecoration:'none', color:'black', fontWeight:'bold'}}> */}
+    //     <OpenModalMenuItem
+    //                 itemText="Yasbnb your home"
+    //                 onItemClick={closeMenu}
+    //                 modalComponent={<CreateSpotModal />}
+    //               /> 
+    //    {/* </NavLink> */}
+    //     </div>
 
-      {isLoaded && (
-        <div id='profBtn' style={{listStyle:'none', marginTop:10,postion:'absolute'}}>
-          <ProfileButton user={sessionUser} />
-        </div>
-      )}
-    </div>
+    //   {isLoaded && (
+    //     <div id='profBtn' style={{listStyle:'none', marginTop:10,postion:'absolute'}}>
+    //       <ProfileButton user={sessionUser} />
+    //     </div>
+    //   )}
+    // </div>
+    // </div>
+    <div>
+          <ul>
+      <li>
+           <NavLink exact to="/" style={{textDecoration:'none'}} id='logo' >
+            {/* TODO: move logo styling to CSS */}
+            <img src ={image} style={{width:120, marginTop:10, cursor:'pointer',marginLeft:30}} alt='logo'/>
+         </NavLink>
+
+      </li>
+      <li>
+        <div className="bnbYourHomeBtn">
+           <OpenModalMenuItem
+                     itemText="Yasbnb your home"
+                     onItemClick={closeMenu}
+                     modalComponent={<CreateSpotModal />}
+                   /> 
+            </div>
+        </li>
+      <li>
+         {isLoaded && (
+         <div className="profileMenuBtn">
+           <ProfileButton user={sessionUser} />
+         </div>)}
+        </li>
+    </ul>
     </div>
   );
 }
