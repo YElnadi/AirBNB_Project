6abef101e16fbc9041072108d1f6b3aff808389f
Yasmine Elnadi <yasmine_adel@hotmail.com>
2023-07-06 14:03:40 -0700

    migration

--- backend/db/seeders/20221024234926-demo-spotImages.js
+++ backend/db/seeders/20221024234926-demo-spotImages.js
@@ -20,101 +20,101 @@ module.exports = {
      return queryInterface.bulkInsert(options, [
        {
          spotId:1,
-         url:"https://a0.muscache.com/im/pictures/miso/Hosting-49924321/original/f0da72bc-28b2-44bd-b3dc-9cb1e1ba946b.jpeg?im_w=1200",
+         url:"https://yasbnb.s3.us-west-2.amazonaws.com/iamge1.webp",
          preview:true
        },
        {
         spotId:2,
-        url:"https://a0.muscache.com/im/pictures/miso/Hosting-30109719/original/601996fa-4df1-4978-a7fd-c56c4078a3b0.jpeg?im_w=1200",
+        url:"https://yasbnb.s3.us-west-2.amazonaws.com/image2.webp",
         preview:true
        },
        {
         spotId:3,
-        url:"https://a0.muscache.com/im/pictures/monet/Select-6603376/original/4fc04ea2-5689-44f5-b7ac-fc39ce8cc32e?im_w=720",
+        url:"https://yasbnb.s3.us-west-2.amazonaws.com/image3.webp",
         preview:true
        },
        {
         spotId:4,
-        url:"https://a0.muscache.com/im/pictures/miso/Hosting-48089233/original/32e6234f-2f32-4b7c-8137-ee81e9f6c7d1.jpeg?im_w=960",
+        url:"https://yasbnb.s3.us-west-2.amazonaws.com/image4.webp",
         preview:true
        },
        {
         spotId:5,
-        url:"https://a0.muscache.com/im/pictures/prohost-api/Hosting-53109080/original/61715bf6-3b87-4a2c-a85c-3f8c49004c93.jpeg?im_w=1200",
+        url:"https://yasbnb.s3.us-west-2.amazonaws.com/image5.webp",
         preview:true
        },
        {
         spotId:6,
-        url:"https://a0.muscache.com/im/pictures/1e16f2f4-1256-44cb-a0f2-85aa57672a45.jpg?im_w=1200",
+        url:"https://yasbnb.s3.us-west-2.amazonaws.com/image6.webp",
         preview:true
        },
        {
        spotId:7,
-       url:"https://a0.muscache.com/im/pictures/401452fe-f779-47aa-a3f6-07875799995b.jpg?im_w=720",
+       url:"https://yasbnb.s3.us-west-2.amazonaws.com/image7.webp",
        preview:true
       },
       {
         spotId:8,
-        url:"https://a0.muscache.com/im/pictures/28c1bd57-9833-4f31-8bcf-52a6dc1d9c06.jpg?im_w=720",
+        url:"https://yasbnb.s3.us-west-2.amazonaws.com/image8.webp",
         preview:true
        },
        {
          spotId:9,
-        url:"https://a0.muscache.com/im/pictures/712a31dd-fad0-4882-ba33-783e5e8620e4.jpg?im_w=720",
+        url:"https://yasbnb.s3.us-west-2.amazonaws.com/image9.webp",
         preview:true
        },
        {
         spotId:10,
-        url:"https://a0.muscache.com/im/pictures/miso/Hosting-44205205/original/191a4a25-0c1d-4405-93dc-21c043c3ccb1.jpeg?im_w=720",
+        url:"https://yasbnb.s3.us-west-2.amazonaws.com/image10.webp",
         preview:true
        },
        {
         spotId:11,
-        url:"https://a0.muscache.com/im/pictures/900639be-e2bc-49cb-a989-cd447f53e03a.jpg?im_w=1200",
+        url:"https://yasbnb.s3.us-west-2.amazonaws.com/image11.webp",
         preview:true
        },
        {
         spotId:12,
-        url:"https://a0.muscache.com/im/pictures/miso/Hosting-12432842/original/a0abf694-f082-4baf-9adc-c7ae93fe025d.jpeg?im_w=1200",
+        url:"https://yasbnb.s3.us-west-2.amazonaws.com/image12.webp",
         preview:true
        },
        {
         spotId:13,
-        url:"https://a0.muscache.com/im/pictures/e1a816a3-2c39-47e4-919d-8880f4eb59c5.jpg?im_w=1200",
+        url:"https://yasbnb.s3.us-west-2.amazonaws.com/image13.webp",
         preview:true
        },
        {
         spotId:14,
-        url:"https://a0.muscache.com/im/pictures/miso/Hosting-49717286/original/f44d0afd-90c6-469d-bed8-bfcaa5ce4f99.jpeg?im_w=1200",
+        url:"https://yasbnb.s3.us-west-2.amazonaws.com/image14.webp",
         preview:true
        },
        {spotId:15,
-        url:"https://a0.muscache.com/im/pictures/miso/Hosting-9131225/original/6c44b60d-aedb-48f4-bb3e-8d78d442c660.jpeg?im_w=1200",
+        url:"https://yasbnb.s3.us-west-2.amazonaws.com/image15.webp",
         preview:true
        },
        {
         spotId:16,
-        url:"https://a0.muscache.com/im/pictures/6b6bd8d9-9734-4925-8f90-2f7806b43c8b.jpg?im_w=1200",
+        url:"https://yasbnb.s3.us-west-2.amazonaws.com/image16.webp",
         preview:true
        },
        {
         spotId:17,
-        url:"https://a0.muscache.com/im/pictures/miso/Hosting-581179430693490727/original/5296ec95-7ba1-4e4c-ae6b-366cf50b1918.jpeg?im_w=1200",
+        url:"https://yasbnb.s3.us-west-2.amazonaws.com/cc3a98b7-d83e-4684-bb03-2b2ce6dd480d.webp",
         preview:true
        },
        {
         spotId:18,
-        url:"https://a0.muscache.com/im/pictures/miso/Hosting-53058812/original/734016f0-6ab8-47b1-9690-b3ca373f6e5d.jpeg?im_w=1200",
+        url:"https://yasbnb.s3.us-west-2.amazonaws.com/image18.webp",
         preview:true
        },
        {
         spotId:19,
-        url:"https://a0.muscache.com/im/pictures/8b29bbe1-fe0a-4a32-9dba-1af15dbde880.jpg?im_w=1200",
+        url:"https://yasbnb.s3.us-west-2.amazonaws.com/image19.webp",
         preview:true
        },
        {
         spotId:20,
-        url:"https://a0.muscache.com/im/pictures/caf13aea-27ca-4134-a45d-c83e89894b04.jpg?im_w=1200",
+        url:"https://yasbnb.s3.us-west-2.amazonaws.com/image20.webp",
         preview:true
        }
 
